f183537f3fd6b548112418ba39338dc17882ecfe
Pete Laurina <plaurina@gmail.com>
2022-02-04 19:00:06 -0500

    fix some sql

--- mysql/createUsers.sql
+++ mysql/createUsers.sql
@@ -11,18 +11,21 @@ DELETE
 FROM rendezvous
 where 1 = 1;
 
+INSERT INTO users (phone, name, coordinator, status, location)
+VALUES ("3035556543", "Stephanie Speck", "9705551234", "UNKNOWN",
+        '{"lat": 39.750652883856255,"lng": -104.99966381517176}'),
+       ("3035559876", "Ben Jabituya", "9705551234", "UNKNOWN", '{"lat": 39.7504993864232,"lng": -104.99989575198671}'),
+       ("3035553219", "Newton Crosby", "9705551234", "UNKNOWN",
+        '{"lat": 39.750571945209856,"lng": -104.99954510156512}'),
+       ("3035552589", "Skroeder", "9705551234", "SAFE", '{"lat": 39.75059691563914,"lng": -104.99955294111153}'),
+       ("3035551478", "Frank", "9705551234", "SAFE", '{"lat": 39.75065645088443,"lng": -104.99919242712184}'),
+       ("3035553698", "Duke", "9705551234", "SAFE", '{"lat": 39.750650511683425,"lng": -104.99917738962986}'),
+       ("3035559632", "Johnny 5", "9705551234", "UNKNOWN", '{"lat": 39.750678143993724,"lng": -104.99917920096432}'),
+       ("3035551111", "Number 1", "9705551234", "VACATION", '{"lat": 21.47264138532755,"lng": -158.03280293887002}');
 
-INSERT INTO users (phone, name, coordinator)
-VALUES ("3035556543", "Stephanie Speck", "9705551234"),
-       ("3035559876", "Ben Jabituya", "9705551234"),
-       ("3035553219", "Newton Crosby", "9705551234"),
-       ("3035552589", "Skroeder", "9705551234"),
-       ("3035551478", "Frank", "9705551234"),
-       ("3035553698", "Duke", "9705551234"),
-       ("3035559632", "Johnny 5", "9705551234"),
-       ("3035551111", "Number 1", "9705551234");
-INSERT INTO users (phone, name, coordinator, isCoordinator)
-VALUES ("9705551234", "Howard Marner", "9705551234", true);
+INSERT INTO users (phone, name, coordinator, isCoordinator, status, location)
+VALUES ("9705551234", "Howard Marner", "9705551234", true, "UNKNOWN",
+        '{"lat": 39.75066992424338, "lng": -104.99908286200353}');
 
 INSERT INTO comments (phone, comment, createdDT)
 VALUES ("3035559876", "I am standing here beside myself.", DATE_SUB(NOW(), INTERVAL 10 MINUTE)),
